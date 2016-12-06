8b3a15c327fde0c3ee95df05cfd0f760dcffe214
Houssam.GHAZALA <jenkins@argus-presse.fr>
2016-12-06 16:31:02 +0100

    Init project.

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    context: __dirname + "/app",
+    entry: "./app.js",
+    output: {
+        path: __dirname + '/app',
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    }
+};
