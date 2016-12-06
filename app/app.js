0159569120b5daeae0d6e71b12a68cc31cfd56c1
Houssam.GHAZALA <jenkins@argus-presse.fr>
2016-12-06 18:09:10 +0100

    Begining with angular component.

--- app/app.js
+++ app/app.js
@@ -2,4 +2,11 @@ var angular = require('angular');
 
 var ngModule = angular.module('app', []);
 
-ngModule.component()
+
+function homeComponent($scope) {
+    var homeCtrl = this;
+    homeCtrl.greeting = 'Hola!';
+}
+
+ngModule.controller('homeComponent', homeComponent);
+
